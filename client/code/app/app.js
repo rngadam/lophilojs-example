e782e10b82ca1080de4423594c6a0bd67ee64fef
Ricky Ng-Adam <rngadam@lophilo.com>
2012-10-24 23:11:23 +0000

    adapt to differential update and full object download

--- client/code/app/app.js
+++ client/code/app/app.js
@@ -12,57 +12,90 @@ function defaultCallback(err, data) {
     else if(data) console.log(data);
 }
   
-ss.event.on('update', function(update) {
+ss.event.on('update', function(updates) {
   console.log('client received update ' + JSON.stringify(update));
-  for (var port in update) {
-    console.log('client updating port ' + port + ' to value ' + update[port]);
-    model[port].value(update[port]);
+  for (var i in updates) {
+    var update = updates[i];
+    console.log('client updating port ' + update.path + ' to value ' + update.value);
+    
+    findObj(model, update.path).last(update.value);
   }
 });
+
+function findObj(obj, objPath) {
+  var start = obj;
+	objPath = objPath.split('.');
+	for(var i in objPath) {
+		if(obj[objPath[i]]) {
+			obj = obj[objPath[i]];
+		}
+	}
+	if(obj !== start) {
+		return obj;
+	} else {
+		return null;
+	}
+}
 
 // we want to preserve the value of port at creation, so 
 // we need to create the function inside another function with the 
 // proper port as parameter
-function makeComputedValue(portname) {
+function makeComputedValue(objPath) {
   return ko.computed(
     function() { 
-      return this[portname].value(); 
+      var obj = findObj(model, objPath);
+//      console.log(obj.path());
+//      console.log(obj.name());
+      return obj.last(); 
     }, 
     model);
 }
 
-function makeComputedLabel(portname) {
-  return ko.computed( 
-      function() {
-        var prefix = this[portname].id() < 10 ? '0' : '';
-        return this[portname].shield() + prefix + this[portname].id();
-      }, 
-      model);  
-}
-
-function makeSimpleLabel(regname) {
-  return ko.computed(
-    function() { 
-      return regname; 
-    }, model);
-}
-
-function dependentPort(portname, port) {
-  // shallow copy
-  port = jQuery.extend({}, port); 
-  port.value = makeComputedValue(portname);
-  port.label = makeComputedLabel(portname);
-  return port;
-}
-
-function dependentRegister(regname, reg) {
+function dependentRegister(objPath, reg) {
   // shallow copy
   reg = jQuery.extend({}, reg); 
-  reg.value = makeComputedValue(regname);
-  reg.label = makeSimpleLabel(regname);
+  reg.last = makeComputedValue(objPath);
   return reg;
 }
 
+function recurseObject(object, objectFunction, path) {
+  var properties = Object.getOwnPropertyNames(object);
+	if(!path) {
+		path = [];
+	}
+	for(var i in properties) {
+		var propertyName = properties[i];
+		var obj = object[propertyName];
+		path.push(propertyName);
+		if (obj !== null && typeof obj == 'object') {
+			objectFunction(obj, path);
+			recurseObject(obj, objectFunction, path);
+		}
+		path.pop();
+	}
+}
+
+function comparator(left, right) {
+  return left.id === right.id ? 0 : (left.id < right.id ? -1 : 1);
+}
+
+function iterateObject(object, fnc) {
+  var properties = Object.getOwnPropertyNames(object);
+	for(var i in properties) {
+		var propertyName = properties[i];
+		var obj = object[propertyName];
+		fnc(obj, propertyName);
+	}
+}
+
+function multiset(value, context) {
+  iterateObject(context, function(register) {
+    if(!register.path) return;
+    if(register.last() === value) return;
+    console.log('client writing %s value %s', register.path, value);    
+    ss.rpc('lophilo.write', register.path, value, defaultCallback);
+  });
+}
 
 ss.rpc('lophilo.load', function(err, data) {
   console.log('loading data');
@@ -74,26 +107,60 @@ ss.rpc('lophilo.load', function(err, data) {
   model.ah = ko.observableArray();
   model.aregs = ko.observableArray();
   
-  for(var portname in data) {
-    var port = data[portname];
-    if(port.type == 'IO') {
-      model.pins.push(dependentPort(portname, port));
-      if(port.side == 'L') {
-        model.al.push(dependentPort(portname, port));
-      } else {
-        model.ah.push(dependentPort(portname, port));
-      }
-    } else {
-      console.log('register: ' + portname);
-      model.aregs.push(dependentRegister(portname, port));
+  model.pwms = ko.observableArray();
+  model.bl = ko.observableArray();
+  model.bh = ko.observableArray(); 
+  
+  model.powerstatus = ko.observable(false);
+  
+  recurseObject(data, function(register) {
+    if(!register.id) {
+      return;
     }
-  }
-  model.toggle = function(port) {
-    var targetValue = port.value() == 1 ? 0 : 1;
-    console.log('client writing %s from value %s to %d', port.name, port.value(), targetValue);
-    ss.rpc('lophilo.write', port.name, targetValue, defaultCallback);
-  };
+    if(register.rtype == 'IO') {
+      var dpa = dependentRegister(register.path, register);
+      model.pins.push(dpa);
+      if(register.side == 'L') {
+        model.al.push(dpa);
+      } else {
+        model.ah.push(dpa);
+      }
+    } else if(register.rtype == 'REGISTER') {
+      if(register.shield == 'A') {
+        console.log('register: ' + register.path);
+        model.aregs.push(dependentRegister(register.path, register));  
+      } else {
+        var dpb = dependentRegister(register.path, register);
+        model.pwms.push(dpb); 
+        if(register.side == 'L') {
+          model.bl.push(dpb);
+        } else {
+          model.bh.push(dpb);
+        }        
+      }
+    }
+  });
+  
+  model.al.sort(comparator);
+  model.ah.sort(comparator);
+  model.bl.sort(comparator);
+  model.bh.sort(comparator);
 
+  model.toggle = function(register) {
+    var targetValue = register.last() === 0 ? 1 : 0;
+    console.log('client writing %s from value %s to %d', register.path, register.last(), targetValue);
+    ss.rpc('lophilo.write', register.path, targetValue, defaultCallback);
+  };
+  
+  model.power = function() {    
+    ss.rpc('lophilo.power', !model.powerstatus(), function() {
+      console.log('power!');
+      model.powerstatus(!model.powerstatus());
+    });
+  };
+  
+  model.allones = multiset.bind(null, 1);
+  model.allzeros = multiset.bind(null, 0);
   ko.applyBindings(model);
 
 });
--- server/rpc/lophilo.js
+++ server/rpc/lophilo.js
@@ -2,97 +2,40 @@
 
 var lophilo = require('lophilo');
 
-var local = {}
-local.gpio0 = {}
-
-function updater(ss, res) {
-    var update = {};
-    for (var port in lophilo.gpio0) {
-      var currentValue = lophilo.gpio0[port].read();
-      if(!local.gpio0[port]) {
-        continue; // wait for load to create ports...
-      }
-      if (local.gpio0[port].value != currentValue) {
-        local.gpio0[port].value = currentValue;
-        update[port] = currentValue;
-      }
-    }
-    if(Object.keys(update).length) {     
-      ss.publish.all('update', update);
-      if(res)
-        res(null, "Server sent events for update");
-    } else {
-      if(res)
-        res(null, null);        
-    }
-}
-
-function createPortObject(shield, port, value) {
-  var data = {};
-  data.name = port;
-  data.value = value;
-  data.shield = shield;
-  if(port.indexOf('io') === 0) {
-    data.type = 'IO';
-    data.id = parseInt(port.slice(2), 10);
-    if(data.id <= 13) {
-      data.side = 'L';
-    } else {
-      data.side = 'H';
-    }
-  } else {
-    data.type = 'REGISTER';
-    data.id = port;
-  }
-  return data;
-}
 
 exports.actions = function(req, res, ss) {
-  //req.use('debug');
-  
-  clearInterval(updater);
-  setInterval(updater.bind(null, ss), 100);
-  
+  //req.use('debug');  
+
   return {
     load: function() {
-      if(!Object.keys(local.gpio0).length) {
-        for (var port in lophilo.gpio0) {
-          if(!local.gpio0[port]) {
-            local.gpio0[port] = createPortObject('A', port, lophilo.gpio0[port].read());
-          } else {
-            local.gpio0[port].value = lophilo.gpio0[port].read();
-          }
-        }
-      }
-      res(null, local.gpio0);
+      lophilo.removeAllListeners('gpio.io');
+      lophilo.unwatch(/gpio0.io/);      
+      lophilo.on('gpio.io', function(update) {
+        ss.publish.all('update', update);
+      });
+      lophilo.watch('gpio.io', /gpio0.io/);      
+      lophilo.readAll(function() {
+        res(null, lophilo);
+      });
     },
-    update: function() {
-      updater(res);
+    write: function(path, value) {
+      if(!path) res('no path!');
+      if(!value) res('no value!');
+      lophilo.write(path, value, res); 
     },
-    write: function(name, value) {
-      lophilo.gpio0[name].write(value);  
-      setTimeout(updater.bind(null, ss), 0);
-      res(null, "Server wrote the value");
+    read: function(path) {
+      if(!path) res('no path!');
+      lophilo.read(path, res); 
     },
     power: function(status) {
       if(status) {
-        lophilo.power.write(lophilo.SHIELDS_POWER_ON);
+        lophilo.powerOnShields();
+
         res(null, 'Powered on');
       } else {
-        lophilo.power.write(lophilo.SHIELDS_POWER_OFF);
+        lophilo.powerOffShields();
         res(null, 'Powered off');
       }
-    },
-    doe: function() {
-      lophilo.gpio0.doe.write(lophilo.GPIO_ALL_ON);
-      res(null, 'Operation completed');      
-    },
-    ledwrite: function(id, value) {
-      lophilo['led' + id].srgb.write(value);
-    },
-    ledread: function(id, value) {
-      res(null, lophilo['led' + id].srgb.read());
-    }
-    
+    }    
   };
 };
