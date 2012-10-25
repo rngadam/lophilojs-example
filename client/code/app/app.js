fe883c10297abc73c1ada31b82b8ba88b1620383
Ricky Ng-Adam <rngadam@lophilo.com>
2012-10-25 12:02:30 +0000

    add power button and some validation server-side

--- client/code/app/app.js
+++ client/code/app/app.js
@@ -5,7 +5,98 @@
 
 console.log('App Loading');
 
+function LophiloModel(data) {
+  var self = this;
+  
+  // we want to preserve the value of port at creation, so 
+  // we need to create the function inside another function with the 
+  // proper port as parameter
+  function makeComputedValue(objPath) {
+    return ko.computed(
+      function() { 
+        var obj = findObj(self, objPath);
+        return obj.last(); 
+      }, 
+      self);
+  }
+  
+  function dependentRegister(objPath, reg) {
+    // shallow copy
+    reg = jQuery.extend({}, reg); 
+    reg.last = makeComputedValue(objPath);
+    return reg;
+  }
+  
+  console.log('loading data');
+  console.dir(data);
+  ko.mapping.fromJS(data, {}, self);
+  console.dir(self);
+  self.pins = ko.observableArray();
+  self.al = ko.observableArray();
+  self.ah = ko.observableArray();
+  self.aregs = ko.observableArray();
+  
+  self.pwms = ko.observableArray();
+  self.bl = ko.observableArray();
+  self.bh = ko.observableArray(); 
+  
+  recurseObject(data, function(register) {
+    
+    if(!register.id) {
+      return;
+    }
+    if(register.rtype == 'IO') {
+      var dpa = dependentRegister(register.path, register);
+      self.pins.push(dpa);
+      if(register.side == 'L') {
+        self.al.push(dpa);
+      } else {
+        self.ah.push(dpa);
+      }
+    } else if(register.rtype == 'REGISTER') {
+      if(register.shield == 'A') {
+        console.log('register: ' + register.path);
+        self.aregs.push(dependentRegister(register.path, register));  
+      } else {
+        var dpb = dependentRegister(register.path, register);
+        self.pwms.push(dpb); 
+        if(register.side == 'L') {
+          self.bl.push(dpb);
+        } else {
+          self.bh.push(dpb);
+        }        
+      }
+    }
+  });
+  
+  self.al.sort(comparator);
+  self.ah.sort(comparator);
+  self.bl.sort(comparator);
+  self.bh.sort(comparator);
+
+  self.toggle = function(register) {
+    var targetValue = register.last() === 0 ? 1 : 0;
+    console.log('client writing %s from value %s to %d', register.path, register.last(), targetValue);
+    ss.rpc('lophilo.write', register.path, targetValue, defaultCallback);
+  };
+  self.powerstatus = ko.computed({
+    read: function() {
+      return self.power.last() === self.SHIELDS_POWER_ON();
+    },
+    write: function(value) {
+      ss.rpc('lophilo.power', !self.power.last(), defaultCallback);
+    }
+  }, self);  
+  self.allones = multiwrite.bind(null, 1);
+  self.allzeros = multiwrite.bind(null, 0);
+}
+
 var model;
+
+ss.rpc('lophilo.load', function(err, data) {
+  model = new LophiloModel(data);
+  ko.applyBindings(model);
+});
 
 function defaultCallback(err, data) {
     if(err) console.log('ERROR: ' + err);
@@ -13,17 +104,26 @@ function defaultCallback(err, data) {
 }
   
 ss.event.on('update', function(updates) {
-  console.log('client received update ' + JSON.stringify(update));
+  console.log('client received update ' + JSON.stringify(updates));
+  if(!model) {
+   console.log('event received before model created!');
+  }
   for (var i in updates) {
     var update = updates[i];
     console.log('client updating port ' + update.path + ' to value ' + update.value);
     
-    findObj(model, update.path).last(update.value);
+    var obj = findObj(model, update.path);
+    if(obj) 
+      obj.last(update.value);
+    else
+      console.error('could not find ' + update.path + ' from update ');
   }
 });
 
 function findObj(obj, objPath) {
   var start = obj;
+  if(!objPath.split)
+    throw new Error('invalid path ' + JSON.stringify(objPath));
 	objPath = objPath.split('.');
 	for(var i in objPath) {
 		if(obj[objPath[i]]) {
@@ -35,27 +135,6 @@ function findObj(obj, objPath) {
 	} else {
 		return null;
 	}
-}
-
-// we want to preserve the value of port at creation, so 
-// we need to create the function inside another function with the 
-// proper port as parameter
-function makeComputedValue(objPath) {
-  return ko.computed(
-    function() { 
-      var obj = findObj(model, objPath);
-//      console.log(obj.path());
-//      console.log(obj.name());
-      return obj.last(); 
-    }, 
-    model);
-}
-
-function dependentRegister(objPath, reg) {
-  // shallow copy
-  reg = jQuery.extend({}, reg); 
-  reg.last = makeComputedValue(objPath);
-  return reg;
 }
 
 function recurseObject(object, objectFunction, path) {
@@ -93,80 +172,11 @@ function multiwrite(value, context) {
   iterateObject(context, function(register) {
     if(!register.path) return;
     if(register.last() === value) return;
-    console.log('client writing %s value %s', register.path, value);    
     updates.push({path: register.path, value: value});
   });
   if(updates.length) {
     ss.rpc('lophilo.multiwrite', updates, defaultCallback);
   }
 }
-
-ss.rpc('lophilo.load', function(err, data) {
-  console.log('loading data');
-  console.dir(data);
-  model = ko.mapping.fromJS(data);
-  console.dir(model);
-  model.pins = ko.observableArray();
-  model.al = ko.observableArray();
-  model.ah = ko.observableArray();
-  model.aregs = ko.observableArray();
-  
-  model.pwms = ko.observableArray();
-  model.bl = ko.observableArray();
-  model.bh = ko.observableArray(); 
-  
-  model.powerstatus = ko.observable(false);
-  
-  recurseObject(data, function(register) {
-    if(!register.id) {
-      return;
-    }
-    if(register.rtype == 'IO') {
-      var dpa = dependentRegister(register.path, register);
-      model.pins.push(dpa);
-      if(register.side == 'L') {
-        model.al.push(dpa);
-      } else {
-        model.ah.push(dpa);
-      }
-    } else if(register.rtype == 'REGISTER') {
-      if(register.shield == 'A') {
-        console.log('register: ' + register.path);
-        model.aregs.push(dependentRegister(register.path, register));  
-      } else {
-        var dpb = dependentRegister(register.path, register);
-        model.pwms.push(dpb); 
-        if(register.side == 'L') {
-          model.bl.push(dpb);
-        } else {
-          model.bh.push(dpb);
-        }        
-      }
-    }
-  });
-  
-  model.al.sort(comparator);
-  model.ah.sort(comparator);
-  model.bl.sort(comparator);
-  model.bh.sort(comparator);
-
-  model.toggle = function(register) {
-    var targetValue = register.last() === 0 ? 1 : 0;
-    console.log('client writing %s from value %s to %d', register.path, register.last(), targetValue);
-    ss.rpc('lophilo.write', register.path, targetValue, defaultCallback);
-  };
-  
-  model.power = function() {    
-    ss.rpc('lophilo.power', !model.powerstatus(), function() {
-      console.log('power!');
-      model.powerstatus(!model.powerstatus());
-    });
-  };
-  
-  model.allones = multiwrite.bind(null, 1);
-  model.allzeros = multiwrite.bind(null, 0);
-  ko.applyBindings(model);
-
-});
 
 console.log('App Loaded');
--- server/rpc/lophilo.js
+++ server/rpc/lophilo.js
@@ -2,35 +2,38 @@
 
 var lophilo = require('lophilo');
 
-
 exports.actions = function(req, res, ss) {
   //req.use('debug');  
-
+  
+  // all updates sent to the UI
+  function passThroughUpdateCallback(update) {    
+    if(typeof update === 'object')
+      ss.publish.all('update', update);
+    else 
+      console.error('invalid update given, got: ' + update);
+  }
   return {
     load: function() {
-      lophilo.removeAllListeners('gpio.io');
-      lophilo.unwatch(/gpio0.io/);      
-      lophilo.on('gpio.io', function(update) {
-        ss.publish.all('update', update);
-      });
-      lophilo.watch('gpio.io', /gpio0.io/);      
+      lophilo.watch(/gpio0.io/);
+      lophilo.watch(/power/);       
+      lophilo.removeAllListeners('update');      
+      lophilo.on('update', passThroughUpdateCallback);
       lophilo.readAll(function() {
         res(null, lophilo);
       });
     },
     write: function(path, value) {
-      if(!path) res('no path!');
-      if(!value) res('no value!');
+      if(!path) res('no path given for write');
+      if(!value) res('no value given for write');
       lophilo.write(path, value, res); 
     },
     read: function(path) {
-      if(!path) res('no path!');
+      if(!path) res('no path given for read');
       lophilo.read(path, res); 
     },
     power: function(status) {
       if(status) {
         lophilo.powerOnShields();
-
         res(null, 'Powered on');
       } else {
         lophilo.powerOffShields();
